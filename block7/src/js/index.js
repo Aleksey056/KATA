06bc92c334cf2c21614d12c7b55cdde01ad42a3a
Aleksey Krendukov <aikrendukov@gmail.com>
2025-05-18 02:59:55 +0500

    swiper-JS

--- block7/src/js/index.js
+++ block7/src/js/index.js
@@ -120,15 +120,15 @@ console.log('ТЕСТ - выполнено');
 
 
 
-// const expandBrands = document.getElementById('brandsList');
-// const expandButtonText = document.getElementById('expandButtonText');
+const expandBrands = document.getElementById('brandsList');
+const expandButtonText = document.getElementById('expandButtonText');
 
-// expandButtonText.addEventListener('click', function () {
-// 	expandBrands.classList.toggle('brands__list--expand');
-// 	expandButton.classList.toggle('expand-button--expand');
-// 	if (expandBrands.classList.contains('brands__list--expand')) {
-// 		expandButtonText.textContent = 'Скрыть';
-// 	} else {
-// 		expandButtonText.textContent = 'Показать всё';
-// 	}
-// });
+expandButtonText.addEventListener('click', function () {
+	expandBrands.classList.toggle('brands__list--expand');
+	expandButton.classList.toggle('expand-button--expand');
+	if (expandBrands.classList.contains('brands__list--expand')) {
+		expandButtonText.textContent = 'Скрыть';
+	} else {
+		expandButtonText.textContent = 'Показать всё';
+	}
+});
